2945500026ddae9e86e0e9b10336e1dce552e5e4
Tomiwa Adelae <thetommedia@gmail.com>
2024-05-15 22:03:08 +0100

    Started the contact page

---
+++ frontend/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Showcase } from "./_components/Showcase";
+
+const page = () => {
+	return (
+		<div>
+			<Showcase />
+		</div>
+	);
+};
+
+export default page;
