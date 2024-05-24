a7fd8435336ca1c67eb2bbc64b7f73543bb74c3f
Tomiwa Adelae <thetommedia@gmail.com>
2024-05-24 02:09:13 +0100

    Made the sameSite equals none

--- utils/generateToken.ts
+++ utils/generateToken.ts
@@ -9,7 +9,7 @@ const generateToken = (res: Response, userId: object) => {
 	res.cookie("jwt", token, {
 		httpOnly: true,
 		secure: process.env.NODE_ENV! !== "development",
-		sameSite: "strict",
+		sameSite: "none",
 		maxAge: 14 * 24 * 60 * 60 * 1000,
 	});
 };
