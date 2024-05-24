12798cb116fe0c54286833ffafd5762e7c690c46
Tomiwa Adelae <thetommedia@gmail.com>
2024-05-24 02:32:24 +0100

    Made the secure in cookies true

--- utils/generateToken.ts
+++ utils/generateToken.ts
@@ -8,7 +8,8 @@ const generateToken = (res: Response, userId: object) => {
 
 	res.cookie("jwt", token, {
 		httpOnly: true,
-		secure: process.env.NODE_ENV! !== "development",
+		// secure: process.env.NODE_ENV! !== "development",
+		secure: true,
 		sameSite: "none",
 		maxAge: 14 * 24 * 60 * 60 * 1000,
 	});
